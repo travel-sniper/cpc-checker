5f8fd228ac32a516993e37f2859557db2f43c12b
Marco Baldanza <marco@X303>
2019-09-03 22:57:44 +0100

    base and home templates with page request example

--- cpc_checker/views.py
+++ cpc_checker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
-
+import requests
 
 def home(request):
     return render(request, 'home.html')
@@ -7,4 +7,7 @@ def home(request):
 
 def test_cpc_cost(request):
     if request.POST:
-        pass
+        url = "https://cpc.farnell.com/duratool/d03214/18v-combi-hammer-drill-2ah-battery/dp/TL19684"
+        header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"}
+        page = requests.get(url, headers=header)
+        print(page.content)
