336c8f8e07a841e0043e16e9ce181c24ee66f6e4
Marco Baldanza <marco@X303>
2019-09-11 20:10:09 +0100

    this is how you do it

--- cpc_checker/views.py
+++ cpc_checker/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect, HttpResponse
 import requests
 
+
 def home(request):
     return render(request, 'home.html')
 
 
-def test_cpc_cost(request):
+def get_cpc_costs(request):
     if request.POST:
-        url = "https://cpc.farnell.com/duratool/d03214/18v-combi-hammer-drill-2ah-battery/dp/TL19684"
-        header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"}
-        page = requests.get(url, headers=header)
-        print(page.content)
+        print(request.POST)
+        # url = "https://cpc.farnell.com/duratool/d03214/18v-combi-hammer-drill-2ah-battery/dp/TL19684"
+        # header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"}
+        # page = requests.get(url, headers=header)
+        # print(page.content)
--- cpcchecker/urls.py
+++ cpcchecker/urls.py
@@ -4,5 +4,6 @@ from cpc_checker import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('get_cpc_costs', views.get_cpc_costs, name='get_cpc_costs'),
     path('admin/', admin.site.urls),
 ]
